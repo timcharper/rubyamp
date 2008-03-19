b4c1a4f2343f13707b5f0524b4ffbf597b0d593f
timcharper <timcharper@5d95301e-d744-0410-833b-1f084d6e47ab>
2008-03-19 17:08:52 +0000

    updated borrowed RSpec bundle components (for spec debugger) to latest
    git-svn-id: https://productivity-bundle.googlecode.com/svn/ProductivityBundle.tmbundle@12 5d95301e-d744-0410-833b-1f084d6e47ab

--- Support/lib/spec/mate.rb
+++ Support/lib/spec/mate.rb
@@ -6,12 +6,14 @@ if File.directory?(rspec_rails_plugin)
   $LOAD_PATH.unshift(rspec_rails_plugin)
 elsif ENV['TM_RSPEC_HOME']
   rspec_lib = File.join(ENV['TM_RSPEC_HOME'], 'lib')
-  raise "TM_RSPEC_HOME points to a bad location: #{ENV['TM_RSPEC_HOME']}" unless File.directory?(rspec_lib)
+  unless File.directory?(rspec_lib)
+    raise "TM_RSPEC_HOME points to a bad location: #{ENV['TM_RSPEC_HOME']}"
+  end
   $LOAD_PATH.unshift(rspec_lib)
 end
 require 'spec'
 
 $LOAD_PATH.unshift(File.dirname(__FILE__) + '/..')
 require 'spec/mate/runner'
-# require 'spec/mate/switch_command'
+require 'spec/mate/switch_command'
 #require 'spec/mate/text_mate_formatter'
