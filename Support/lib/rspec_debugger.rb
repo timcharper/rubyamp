4ccc02da29bd1f71b5825263522f7ce25cab74ae
Tim Harper <timcharper@gmail.com>
2008-09-25 14:31:27 -0600

    Support to debug RSpec story files (overcomes the at_exit issue with ruby-debug)

--- Support/lib/rspec_debugger.rb
+++ Support/lib/rspec_debugger.rb
@@ -1,8 +1,34 @@
 require File.dirname(__FILE__) + "/ruby_amp.rb"
+
+def debug_rspec_story
+  if RubyAMP::Config[:rspec_story_bundle_path].nil?
+    puts "Can't find rspec-story.tmbundle.  Use 'Edit RubyAMP Global Config' to set the path to where it's installed"
+  end
+  
+  Dir.chdir(ENV['TM_PROJECT_DIRECTORY'])
+  wrapper_file = RubyAMP::RemoteDebugger.prepare_debug_wrapper(<<-EOF)
+    ENV['TM_BUNDLE_SUPPORT'] = RubyAMP::Config[:rspec_story_bundle_path] + "/Support"
+    
+    require "#{RubyAMP::Config[:rspec_story_bundle_path]}/Support/lib/spec/mate/story/story_helper"
+    
+    while Debugger.handler.interface.nil?; sleep 0.10; end
+    Spec::Mate::Story::StoryHelper.new(ENV['TM_FILEPATH']).run_story
+    Runner.story_runner.run_stories
+  EOF
+  RubyAMP::Launcher.open_controller_terminal
+
+  ARGV << "-s"
+  ARGV << wrapper_file
+
+  require 'rubygems'
+  require 'ruby-debug'
+  load 'rdebug'
+end
+
 def debug_rspec(focussed_or_file = :file)
   raise "Invalid argument" unless %w[focussed file].include?(focussed_or_file.to_s)
   if RubyAMP::Config[:rspec_bundle_path].nil?
-    puts "Can't find rspec.tmbundle.  Use 'edit global config' and set it there"
+    puts "Can't find rspec.tmbundle.  Use 'Edit RubyAMP Global Config' to set the path to where it's installed"
   end
   
   Dir.chdir(ENV['TM_PROJECT_DIRECTORY'])
